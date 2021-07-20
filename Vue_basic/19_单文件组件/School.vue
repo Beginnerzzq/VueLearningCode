4c310baf1d72e239de686701fcecb3490e7d9b09
Beginnerzzq <307545449@qq.com>
2021-07-20 23:15:00 +0800

    Vue_basic learning code

---
+++ Vue_basic/19_单文件组件/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+    //组件交互相关的代码(数据、方法等等)
+    export default{
+        name:'School',
+        data() {
+            return {
+                name: 'jufe',
+                address: '江西'
+            }
+        },
+        methods: {
+            showName() {
+                console.log('showName', this)
+            }
+        },
+    }
+</script>
+
+<style>
+/* 组件的样式 */
+.demo {
+  background-color: orange;
+}
+</style>
