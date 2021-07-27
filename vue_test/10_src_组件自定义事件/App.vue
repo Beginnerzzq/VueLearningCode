30d36c29b0f1d2f0f0ae10721b84d7b9dc43b930
Beginnerzzq <307545449@qq.com>
2021-07-27 23:59:31 +0800

    Vue脚手架 Learning Code

---
+++ vue_test/10_src_组件自定义事件/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="app">
+        <h1>{{msg}}</h1>
+
+        <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
+        <School :getSchoolName="getSchoolName"/>
+
+        <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据(第一种写法，使用@或v-on) -->
+        <Student @atjufe="getStudentName" @demo="m1"/>
+        <!-- <Student v-on:atjufe="getStudentName"/> -->
+
+        <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据(第二种写法，使用ref) -->
+        <!-- <Student ref="student"/> -->
+    </div>
+</template>
+
+<script>
+    import School from './components/School.vue'
+    import Student from './components/Student.vue'
+
+    export default {
+        name:'App',
+        components:{Student,School},
+        data() {
+            return {
+                msg:'你好'
+            }
+        },
+        methods: {
+            getSchoolName(schoolName){
+                console.log('App收到了学校名:' + schoolName);
+            },
+            getStudentName(studentName){
+                console.log('App收到了学生名:' + studentName);
+            },
+            m1(){
+                console.log('demo事件被触发了');
+            }
+        },
+        mounted() {
+            // this.$refs.student.$on('atjufe',this.getStudentName)
+            // this.$refs.student.$once('atjufe',this.getStudentName)  //只能使用一次
+        },
+    }
+</script>
+
+<style>
+    .app{
+        background-color: gray;
+        padding: 5px;
+    }
+</style>
---
+++ vue_test/10_src_组件自定义事件/components/Student.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="student">
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生性别：{{sex}}</h2>
+        <button @click="sendStudentName">把学生名给App</button>
+        <button @click="unbind">解绑事件</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Student',
+        data() {
+            return {
+                name:'张三',
+                sex:'男'
+            }
+        },
+        methods: {
+            sendStudentName(){
+                //触发Student组件实例身上的atguigu事件
+                this.$emit('atjufe',this.name)
+                // this.$emit('demo')
+            },
+            unbind(){
+                this.$off('atjufe') //解绑一个自定义事件
+                // this.$off(['atjufe','demo']) //解绑全部自定义事件
+            }
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+    .student{
+        background-color: pink;
+        margin-top: 30px;
+    }
+</style>
