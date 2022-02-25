6c228fb2fd02fa4fc34a4447772b477fb1f555bb
Beginnerzzq <307545449@qq.com>
2022-02-25 21:29:20 +0800

    Vue脚手架 learningCode

---
+++ vue_test/30_src_路由的query参数/pages/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <li>消息编号:{{$route.query.id}}</li>
+        <li>消息标题:{{$route.query.title}}</li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Detail',
+        mounted() {
+            console.log(this.$route);
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue_test/30_src_路由的query参数/pages/Message.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="message in messageList" :key="message.id">
+                
+                <!-- 跳转路由并携带query参数，to的字符串写法 -->
+                <!-- <router-link :to="`/home/message/detail?id=${message.id}&title=${message.titile}`">{{message.title}}</router-link>&nbsp;&nbsp; -->
+                
+                <router-link :to="{
+                    path:'/home/message/detail',
+                    query:{
+                        id:message.id,
+                        title:message.title,
+                    }
+                }">
+                    {{message.title}}
+                </router-link>&nbsp;&nbsp;
+            </li>
+        </ul>
+        <hr>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Message',
+        data() {
+            return {
+                messageList:[
+                    {id:'001',title:'消息001'},
+                    {id:'002',title:'消息002'},
+                    {id:'003',title:'消息003'},
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
